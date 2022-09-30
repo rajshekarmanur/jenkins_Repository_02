3773f018ef78f6bdec8bed4233c23f122cf2818b
rajsh <rajsh@DESKTOP-S7F8E9F>
2022-09-30 15:35:47 +0530

    jenkins project commite

---
+++ src/test/java/jenkin_module/Sample01Test.java
@@ -0,0 +1,19 @@
+package jenkin_module;
+
+import org.testng.annotations.Test;
+
+public class Sample01Test {
+	@Test
+	public void create()
+	{
+		System.out.println("----created----");
+		
+	}
+	
+	@Test
+	public void modify()
+	{
+		System.out.println("----modify----");
+	}
+
+}
---
+++ src/test/java/jenkin_module/Sample02Test.java
@@ -0,0 +1,16 @@
+package jenkin_module;
+
+import org.apache.hc.core5.util.Asserts;
+import org.testng.annotations.Test;
+
+public class Sample02Test {
+	@Test
+	public void delete()
+	{
+		System.out.println("----delet----");
+		System.out.println("----delet----");
+		System.out.println("----delet----");
+		
+	}
+
+}
